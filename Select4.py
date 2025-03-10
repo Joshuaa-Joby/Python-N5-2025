b27bd11640e918ae93699d20dcf0d4b45aa29fda
Joshuaa Joby <J.Joby@albynschool.org>
2025-03-10 09:44:26 +0000

    hi

--- Select4.py
+++ Select4.py
@@ -2,7 +2,7 @@
 age = float(input("Please enter your age: "))
 if age < 0.0:
   print("Not possible! You have to be at least 0 years old.")
-elif age < 18.0:
-  print("You cannot apply to college yet.")
+elif age < 17.0:
+  print("You cannot learn to drive a car yet.")
 else:
-  print("You can apply to college.")
+  print("You can learn to drive a car.")
