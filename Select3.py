7fb739cf887acf7f7cb33d7c8e1f692ded714a66
Joshuaa Joby <J.Joby@albynschool.org>
2025-03-10 09:32:43 +0000

    hi

--- Select3.py
+++ Select3.py
@@ -1,6 +1,6 @@
 #Program 3 - Investigate and modify
-luckynumber = int(input("Please enter your lucky number"))
-if luckynumber == 4:
+luckynumber = int(input("Please enter your lucky number:"))
+if luckynumber == 4 or 21:
     print("Amazing! It's mine too!")
 else:
     print("I think you entered your unlucky number")
