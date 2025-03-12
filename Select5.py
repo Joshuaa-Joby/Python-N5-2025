f3d7a6c77dec98dceae8d6633089db7f42365a1b
Joshuaa Joby <J.Joby@albynschool.org>
2025-03-12 14:50:30 +0000

    hi

---
+++ Select5.py
@@ -0,0 +1,11 @@
+NumberOfBadges = int(input("Please enter how many badges you want to order: "))
+PriceOfBadges = float(0.25)
+if  NumberOfBadges > 150:
+  cost = float(NumberOfBadges*PriceOfBadges)
+  discount = float(NumberOfBadges*PriceOfBadges*0.1)
+  print("£"())
+elif NumberOfBadges > 150:
+  sum(150*0.25*0.1)
+  print("£"(sum))
+else:
+  print(".")
---
+++ Select6.py
@@ -0,0 +1,9 @@
+# Program 1 - Predict and read
+message = input("Please enter the message to display: ")
+
+for index in range(0,10):
+  print(message)
+
+
+#a) 10 ten times
+#b) Fixed loop
