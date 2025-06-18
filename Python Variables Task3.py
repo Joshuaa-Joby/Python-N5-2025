da9a3ad004185f2eefc13d55225a6d99f1d99fe9
Joshuaa Joby <J.Joby@albynschool.org>
2025-06-18 14:38:49 +0100

    hi

--- Python Variables Task3.py
+++ Python Variables Task3.py
@@ -1,4 +1,4 @@
-celcius_temp = float(input("Enter temperature in Celsius: "))
-fahrenheit_temp = (celcius_temp * 9/5) + 32
+celsius_temp = float(input("Enter temperature in Celsius: "))
+fahrenheit_temp = (celsius_temp * 9/5) + 32
 print("The temperature in Fahrenheit is: {fahrenheit_temp}°F")
 print("The temperature in Celsius is: {celcius_temp}°C")
