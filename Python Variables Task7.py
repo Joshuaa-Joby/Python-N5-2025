6c3df488dfcda680d491b2f4fd8ed0214a84d87b
Joshuaa Joby <J.Joby@albynschool.org>
2025-06-18 15:11:30 +0100

    hi

--- Python Variables Task7.py
+++ Python Variables Task7.py
@@ -1,6 +1,7 @@
 overdue = int(input("Enter the number of days books overdue: "))  
 fine_per_book = 0.50 
 total_fine = overdue * fine_per_book 
-if total_fine > 5:
-    total_fine += 2.00 
-print("The total fine for overdue books is: £" + str(total_fine))  
+if overdue > 5:
+    total_fine=total_fine + 2.00 
+
+print("The total fine for overdue books is: £" + str(round(total_fine,3)))
